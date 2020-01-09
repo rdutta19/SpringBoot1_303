3caf01d9d986bdb9a0d0b28ac3e795ae3f5233cf
rdutta19 <richadutta19@gmail.com>
2020-01-09 13:07:11 -0500

    SpringBoot

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    @Size(min=4)
+    private String taskname;
+    @DateTimeFormat(pattern="yyyy/MM/dd")
+    private Date duedate;
+    @NotNull
+    @Size(min=8)
+    private String priority;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskname() {
+        return taskname;
+    }
+
+    public void setTaskname(String taskname) {
+        this.taskname = taskname;
+    }
+
+    public Date getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(Date duedate) {
+        this.duedate = duedate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
